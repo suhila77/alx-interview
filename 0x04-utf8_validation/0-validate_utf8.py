e09a35239590934b026360238774b72320de5fc3
suhila77 <suhilab9@gmail.com>
2025-02-05 22:43:28 +0000

    FULL TASKS

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,27 @@
+def validUTF8(data):
+    """
+    Determines if a given data set represents a valid UTF-8 encoding.
+    :param data: List of integers representing bytes.
+    :return: True if data is a valid UTF-8 encoding, else False.
+    """
+    num_bytes = 0
+    
+    for num in data:
+        byte = num & 0xFF  # Consider only the last 8 bits
+        
+        if num_bytes == 0:
+            if (byte >> 5) == 0b110:
+                num_bytes = 1
+            elif (byte >> 4) == 0b1110:
+                num_bytes = 2
+            elif (byte >> 3) == 0b11110:
+                num_bytes = 3
+            elif (byte >> 7):
+                return False
+        else:
+            if (byte >> 6) != 0b10:
+                return False
+            num_bytes -= 1
+    
+    return num_bytes == 0
+
---
+++ 0x04-utf8_validation/README.md
@@ -0,0 +1,1 @@
+hello from 0x04-utf8_validation
